83105d5f6d721b5a91f50f1fb9866260010d75a2
Gio <giovanni.lodi42@gmail.com>
2013-04-25 10:13:41 +0100

    Added first util class: MGInsetLabel

---
+++ ObjectiveUtils/Classes/MGInsetLabel.h
@@ -0,0 +1,16 @@
+//
+//  MGInsetLabel.h
+//  CatAcademy
+//
+//  Created by Giovanni Lodi on 1/28/13.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+//  Based on IPInsetLabel
+//  https://gist.github.com/2596057
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGInsetLabel : UILabel
+@property (nonatomic, assign) UIEdgeInsets insets;
+@end
