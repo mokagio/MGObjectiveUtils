ce31d85af4b9ceb7c1e96aa460b84eced796f858
Gio <giovanni.lodi42@gmail.com>
2013-07-09 17:00:43 +0100

    Added empty project Playground

---
+++ MGObjectiveUtilsPlayground/MGObjectiveUtilsPlayground/MGViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGViewController.h
+//  MGObjectiveUtilsPlayground
+//
+//  Created by Gio on 09/07/2013.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGViewController : UIViewController
+
+@end
