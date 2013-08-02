677ca321568809e611063c2f3ca1ba5870c8d003
Gio <giovanni.lodi42@gmail.com>
2013-08-02 17:32:43 +0100

    Added fresh project for tests

---
+++ MGObjectiveUtilsTests/MGObjectiveUtilsTests/MGViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGViewController.h
+//  MGObjectiveUtilsTests
+//
+//  Created by Gio on 02/08/2013.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGViewController : UIViewController
+
+@end
---
+++ MGObjectiveUtilsTests/MGObjectiveUtilsTestsTests/MGObjectiveUtilsTestsTests.h
@@ -0,0 +1,13 @@
+//
+//  MGObjectiveUtilsTestsTests.h
+//  MGObjectiveUtilsTestsTests
+//
+//  Created by Gio on 02/08/2013.
+//  Copyright (c) 2013 mokagio. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MGObjectiveUtilsTestsTests : SenTestCase
+
+@end
